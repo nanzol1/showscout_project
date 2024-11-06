7a080ed62f7a8d64fd8115252ddd1d24a65f09c6
Dudás Patrícia Dóra <patrisssa@mailbox.unideb.hu>
2024-11-06 23:03:02 +0100

    Homepage-hez view létrehozása

---
+++ project-root/app/Views/user/home.php
@@ -0,0 +1,20 @@
+<div class="container-fluid movie-container">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <div class="movie-card">
+                <div class="movie-image">
+                    <img src="path_to_your_image.jpg" alt="Movie Image">
+                </div>
+                <div class="movie-info">
+                    <div class="movie-heading">
+                    </div>
+                    <div class="movie-release">
+                    </div>
+                    <div class="movie-summary">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
