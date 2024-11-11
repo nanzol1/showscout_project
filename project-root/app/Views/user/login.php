572a997ae104aeed688333b148abc0329d9ad5ee
Kiss Gréta <kgreta0310@gmail.com>
2024-11-11 01:58:10 +0100

    Login view változtatása és style.css módosítása,finomításra szorulnak

--- project-root/app/Views/user/login.php
+++ project-root/app/Views/user/login.php
@@ -1,32 +1,34 @@
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('public/css/style.css') ?>">
-    <title>Bejelentkezés</title>
-</head>
-<body>
-    <div class="container">
-        <h2>Bejelentkezés</h2>
-        <?php if (session()->getFlashdata('error')):?>
-            <div class="alert alert-danger">
-                <?= session()-> getFlashdata('error')?>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-5 pb-5">
+
+            <div class="card shadow-sm">
+                <div class="card-body">
+                    <h3 class="text-center mb-4">Bejelentkezés</h3>
+                    <form action="login/authenticate" method="POST">
+                        <?= csrf_field() ?>
+
+                        <div class="form-group mb-3">
+                            <label for="username">Felhasználónév</label>
+                            <input class="form-control" type="text" id="username" name="username" placeholder="Felhasználónév" required>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="password">Jelszó</label>
+                            <input class="form-control" type="password" id="password" name="password" placeholder="Jelszó" required>
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <button class="btn btn-primary w-100" type="submit">Bejelentkezés</button>
+                        </div>
+
+                    </form>
+
+                    <div class="mt-3 text-center">
+                        <p>Nem rendelkezik még fiókkal? <a href="register">Regisztráljon itt</a></p>
+                    </div>
+                </div>
             </div>
-        <?php endif; ?>
-         <form action="<?= site_url('login/authenticate')?>" method="post">
-            <?=csrf_field()?>
-            <div class="form-group">
-                <label for="username">Felhasználónév</label>
-                <input type="text" id="username" name="username" class="form-control" value="<?= old('username')?>" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Jelszó</label>
-                <input type="password" name="password" id="password" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Bejelentkezés</button>
-         </form>
-         <p>Ha még nincs fiókod, regisztrálj <a href="<?= site_url('register') ?>">itt</a>.</p>
+        </div>
     </div>
-</body>
-</html>
+</div>
