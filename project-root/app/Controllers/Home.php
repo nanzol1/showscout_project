980c817a0a8421a4a10abe98bd081ce7ef9ccf9c
Dudás Patrícia Dóra <patrisssa@mailbox.unideb.hu>
2024-11-25 01:14:54 +0100

    Home oldal alakítása, szűrő hozzáadás, tovább gomb

--- project-root/app/Controllers/Home.php
+++ project-root/app/Controllers/Home.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\Media_model;
+use App\Models\Streamingservice_model;
 use CodeIgniter\Files\File;
 
 class Home extends BaseController
@@ -13,9 +15,56 @@ class Home extends BaseController
         return $this->loadMovies();
     }
 
-    public function loadPage($page = 0, $data = []){
-        return view('templates/header').view($page,$data).view('templates/footer');
+    public function filter()
+    {
+        $type = $this->request->getGet('type');
+        $genre = $this->request->getGet('genre');
+        $providers = $this->request->getGet('providers');
+        $released_from = $this->request->getGet('released_from');
+        $released_to = $this->request->getGet('released_to');
+    
+        $moviesModel = new Media_model();
+        $query = $moviesModel->select('media.*, streamingservices.Name as StreamingProvider') // Lekérjük a StreamingProvider-t
+                             ->distinct()
+                             ->join('mediaxgenre', 'mediaxgenre.MediaID = media.ID', 'left')
+                             ->join('genres', 'genres.ID = mediaxgenre.GenreID', 'left')
+                             ->join('streamingservices', 'streamingservices.ID = media.Ss_id', 'left'); // Csak akkor jelenik meg, ha van Ss_id
+    
+        if ($type) {
+            $query->where('Type', $type);
+        }
+    
+        if ($genre) {
+            $query->where('genres.Genre', $genre); // A genres tábla 'Name' oszlopára szűrni
+        }
+    
+        if ($providers) {
+            $query->whereIn('streamingservices.Name', $providers); // A streamingservices.Name alapján szűrés
+        }
+    
+        if ($released_from) {
+            $query->where('Released >=', $released_from);
+        }
+    
+        if ($released_to) {
+            $query->where('Released <=', $released_to);
+        }
+    
+        $data['movies'] = $query->get()->getResult(); // Lekérdezett adatok
+        $data['type'] = $type;
+        $data['genre'] = $genre;
+        $data['providers'] = $providers;
+        $data['released_from'] = $released_from ?? '';
+        $data['released_to'] = $released_to ?? '';
+    
+        // Szolgáltatók listája
+        $providersModel = new Streamingservice_model();
+        $data['providers_list'] = $providersModel->getAllProviders();
+    
+        return $this->loadPage('user/home', $data);
     }
+    
+
 
     public function loadMovies()
     {
@@ -23,12 +72,30 @@ class Home extends BaseController
         $movies = $moviesModel->getStreaming();
         $data['movies'] = $movies;
 
-        if (empty($data['movies'])) {
-            return $movies = [];
-        }
-
+        // Szolgáltatók listája
+        $providersModel = new Streamingservice_model();
+        $data['providers_list'] = $providersModel->getAllProviders();
 
         return $this->loadPage('user/home', $data);
     }
+
+    public function reszletek($id)
+    {
+        $movieModel = new Media_model();
+        $movie = $movieModel->select('media.*, streamingservices.Name as StreamingProvider, streamingservices.Lowest_price_plan as Price, streamingservices.Link')
+            ->join('streamingservices', 'streamingservices.ID = media.Ss_id')
+            ->where('media.ID', $id)
+            ->first();
+
+        if (!$movie) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException("Movie not found");
+        }
+
+        return $this->loadPage('user/movie_details', ['movie' => $movie]);
+    }
+
+    public function loadPage($page = 0, $data = [])
+    {
+        return view('templates/header') . view($page, $data) . view('templates/footer');
+    }
 }
-?>
--- project-root/app/Views/user/home.php
+++ project-root/app/Views/user/home.php
@@ -1,30 +1,92 @@
 <div class="container-fluid movie-container">
     <div class="row justify-content-center">
         <div class="col-10">
-            <?php foreach($movies as $item):?>
-                <?=var_dump($item)  ?>
+            <?php foreach($movies as $item): ?>
                 <div class="movie-card">
-                <div class="movie-image">
-                <img src="<?=base_url('assets/images/').$item["Img_path"] ?>" alt="Movie pic">
-                </div>
-                <div class="movie-info">
-                    <div class="movie-heading">
-                        <?=$item["Title"] ?>
+                    <div class="movie-image">
+                        <img src="<?= base_url('assets/images/').(is_object($item) ? $item->Img_path : $item['Img_path']) ?>" alt="Movie pic">
                     </div>
-                    <div class="movie-release">
-                        <?=$item["Released"] ?>
-                    </div>
-                    <div class="movie-ss">
-                        <?=$item["StreamingProvider"] ?>
-                    </div>
-                    <div class="movie-summary">
-                        <div class="movie-desc">
-                            <?=$item["Description"] ?>
+                    <div class="movie-info">
+                        <div class="movie-heading">
+                            <?= is_object($item) ? $item->Title : $item['Title'] ?>
+                        </div>
+                        <div class="movie-release">
+                            <?= is_object($item) ? $item->Released : $item['Released'] ?>
+                        </div>
+                        <div class="movie-ss">
+                            <?= is_object($item) ? $item->StreamingProvider : $item['StreamingProvider'] ?>
+                        </div>
+                        <div class="movie-summary">
+                            <div class="movie-desc">
+                                <?= substr(is_object($item) ? $item->Description : $item['Description'], 0, 400) ?>...
+                            </div>
+                            <a href="<?= base_url('film/reszletek/'.(is_object($item) ? $item->ID : $item['ID'])) ?>" class="read-more-link">Tovább</a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
+
+<script>
+    window.addEventListener("scroll", function() {
+        localStorage.setItem("scrollPosition", window.scrollY);
+    });
+
+    window.onload = function() {
+        const cameFromDetails = localStorage.getItem("cameFromDetails");
+
+        if (cameFromDetails === "true") {
+            const scrollPosition = localStorage.getItem("scrollPosition");
+            if (scrollPosition) {
+                window.scrollTo(0, scrollPosition);
+            }
+            localStorage.removeItem("cameFromDetails");
+        }
+    };
+</script>
+
+<form action="<?= base_url('home/filter') ?>" method="GET">
+    <div class="filter-container">
+        
+        <label for="type">Típus:</label>
+        <select id="type" name="type">
+            <option value="">-- Válassz típust --</option>
+            <option value="film" <?= (isset($type) && $type == 'film') ? 'selected' : '' ?>>Film</option>
+            <option value="series" <?= (isset($type) && $type == 'series') ? 'selected' : '' ?>>Sorozat</option>
+        </select>
+
+        
+        <label for="genre">Kategória:</label>
+        <select id="genre" name="genre">
+            <option value="">-- Válassz kategóriát --</option>
+            <option value="Action" <?= (isset($genre) && $genre == 'Action') ? 'selected' : '' ?>>Akció</option>
+            <option value="Comedy" <?= (isset($genre) && $genre == 'Comedy') ? 'selected' : '' ?>>Vígjáték</option>
+            <option value="Drama" <?= (isset($genre) && $genre == 'Drama') ? 'selected' : '' ?>>Dráma</option>
+            <option value="Horror" <?= (isset($genre) && $genre == 'Horror') ? 'selected' : '' ?>>Horror</option>
+            <option value="Thriller" <?= (isset($genre) && $genre == 'Thriller') ? 'selected' : '' ?>>Thriller</option>
+        </select>
+
+        
+        <label for="providers">Szolgáltató:</label>
+        <div id="providers">
+            <?php if (isset($providers_list)): ?>
+                <?php foreach ($providers_list as $provider): ?>
+                    <label>
+                        <input type="checkbox" name="providers[]" value="<?= $provider['Name'] ?>" <?= (isset($providers) && in_array($provider['Name'], $providers)) ? 'checked' : '' ?>> <?= $provider['Name'] ?>
+                    </label><br>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
+
+
+        <label for="released_from">Megjelenés dátuma:</label>
+        <div class="date-range">
+            <input type="date" id="released_from" name="released_from" value="<?= esc($released_from ?? '') ?>" />
+            <input type="date" id="released_to" name="released_to" value="<?= esc($released_to ?? '') ?>" />
+        </div>
+
+        <button type="submit">Szűrés</button>
+    </div>
+</form>
